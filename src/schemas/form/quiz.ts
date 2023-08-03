ad5e3c5e56889bcc82dd3ff51cf256c2bbb5b438
jonathangorbachev <jonathangorbachev@gmail.com>
2023-08-03 19:10:42 -0400

    trying to make api work :\

--- src/schemas/form/quiz.ts
+++ src/schemas/form/quiz.ts
@@ -1,12 +1,14 @@
-// validate any input from form
 import { z } from "zod";
 
-// defining what the form will accept
 export const quizCreationSchema = z.object({
-    // topic of the input will be a string, 4-50 characteres inclusive, extra param that will be an error message
-    topic: z.string().min(4, {message: "Topic must be at least 4 characters long"}).max(50),
-    // types of the questions generated, multiple choice question or open ended question
-    type: z.enum(['mcq', 'open_ended']),
-    // number of questions we want to generate to the user, 1-10 inclusive
-    amount: z.number().min(1).max(10)
-})
+  topic: z
+    .string()
+    .min(4, {
+      message: "Topic must be at least 4 characters long",
+    })
+    .max(50, {
+      message: "Topic must be at most 50 characters long",
+    }),
+  type: z.enum(["mcq", "open_ended"]),
+  amount: z.number().min(1).max(10),
+});
